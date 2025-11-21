5e09bfba36872fe769616850fb9c414febdc694d
VeraWatermelon <19730032635@163.com>
2025-11-22 00:15:11 +0800

    电机初始化及电机调速功能

---
+++ Hardware/Motor.h
@@ -0,0 +1,8 @@
+#ifndef __Motor_H
+#define __Motor_H
+
+void Motor_Init(void);
+void MotorA_SetSpeed(int8_t Speed);
+void MotorB_SetSpeed(int8_t Speed);
+
+#endif
--- User/setoff.c
+++ User/setoff.c
@@ -1,6 +1,8 @@
 #include "stm32f10x.h"                  // Device header  
+#include "Motor.h"  
 void Setoff(void)
 {
+	Motor_Init();
 	while(1)
 	{
 		
